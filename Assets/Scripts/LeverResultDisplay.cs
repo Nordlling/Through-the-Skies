7d5e652a8779668e464757489074cf3052b78d0c
apodvorny <apodvorny@devsgroup.net>
2023-04-10 09:32:42 +0300

    Add UI for player death(test)

---
+++ Assets/Scripts/LeverResultDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LeverResultDisplay : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void Start()
+    {
+        Invoke(nameof(test), 6f);
+    }
+
+    private void test()
+    {
+        animator.SetTrigger("fail");
+    }
+}
